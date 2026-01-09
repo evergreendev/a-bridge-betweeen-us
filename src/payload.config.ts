fa747f32c7d3f949604019bc283ffcf02313ecb8
Joe simpson <joe@egmrc.com>
2026-01-09 16:25:57 -0700

    chore: update pnpm-lock.yaml with latest dependencies
    - Upgraded Payload CMS packages to `3.70.0`, including new dependencies such as `@payloadcms/email-nodemailer`.
    - Updated related dependencies, including `@dnd-kit`, `drizzle-orm`, `next`, and others, ensuring compatibility with the latest versions.
    - Reflected dependency updates across `pnpm-lock.yaml`.

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,5 +1,6 @@
 // storage-adapter-import-placeholder
 import { postgresAdapter } from '@payloadcms/db-postgres'
+import { nodemailerAdapter } from '@payloadcms/email-nodemailer'
 
 import sharp from 'sharp' // sharp-import
 import path from 'path'
@@ -57,6 +58,19 @@ export default buildConfig({
   },
   // This config helps us configure global or default features that the other editors can inherit
   editor: defaultLexical,
+  email: nodemailerAdapter({
+    defaultFromAddress: process.env.SMTP_USER || 'abridgebetweenus@mail.egmrc.com',
+    defaultFromName: 'A Bridge Between Us',
+    transportOptions: {
+      host: process.env.SMTP_HOST,
+      port: Number(process.env.SMTP_PORT),
+      secure: Number(process.env.SMTP_PORT) === 465, // true for port 465, false for other ports
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    },
+  }),
   db: postgresAdapter({
     pool: {
       connectionString: process.env.DATABASE_URI || '',
